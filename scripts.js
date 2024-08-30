31c4758350efa933e78697c3b7d961c25c40101a
Vasim7989 <vasimakram2110@gmail.com>
2024-08-30 11:14:39 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
     alert("Thank you for your message! I'll get back to you soon.");
-    // Here you can add AJAX code to send form data to your email or server.
+    
 });
